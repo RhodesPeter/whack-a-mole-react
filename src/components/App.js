927d7dd963e68f18e9a38124e9368f6284d3af93
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-20 00:57:40 +0100

    Add moles to DOM!

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
-// import logo from '../logo.svg';
 import '../App.css';
 
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.displayMoles();
+  }
+
+  displayMoles(){
+    const moles = document.querySelectorAll('.game__mole');
+    const game = document.getElementsByTagName('div');
+    const mole = document.getElementsByClassName('.game__mole')[0];
+    console.log(moles, game, mole);
+    return Math.floor(Math.random() * moles.length + 1);
+  }
+
   render() {
     return (
       <div className="main-container">
         <div className="game">
           <h1 className="game__title">WHACK-A-MOLE</h1>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
-          <div className="game__hole"></div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
+          <div className="game__hole">
+            <div className="game__mole"></div>
+          </div>
         </div>
       </div>
-
-      // <div className="App">
-      //   <div className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h2>Welcome to React</h2>
-      //   </div>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
     );
   }
 }

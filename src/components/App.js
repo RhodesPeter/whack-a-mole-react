3187e46359881531169420011ce10587f64557a5
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-20 17:09:59 +0100

    Add functions inline!

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-
 class App extends Component {
 
-  constructor(){
-    super();
-    this.displayMoles();
-  }
+  // constructor(){
+  //   super();
+  // }
 
   displayMoles(){
-    const moles = document.querySelectorAll('.game__mole');
-    const game = document.getElementsByTagName('div');
-    const mole = document.getElementsByClassName('.game__mole')[0];
-    console.log(moles, game, mole);
-    return Math.floor(Math.random() * moles.length + 1);
+    console.log(Math.floor(Math.random() * 10))
+    return Math.floor(Math.random() * 10);
   }
 
   render() {
     return (
       <div className="main-container">
+        <button type="button" onClick={() => this.displayMoles()}>Click Me!</button>
         <div className="game">
           <h1 className="game__title">WHACK-A-MOLE</h1>
           <div className="game__hole">

07f673b10d9847152aa4fe1fefd4e81ac761b9ec
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-24 22:48:35 +0100

    Add mole hills

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../App.css';
-
 class App extends Component {
 
   constructor(props, context) {
@@ -16,6 +15,7 @@ class App extends Component {
       7:'translate(0, 110%)',
       8:'translate(0, 110%)',
       9:'translate(0, 110%)',
+      shake: 'translate(0, 0)',
       gameHasStarted: false,
       moleHasBeenWhacked: false,
       score: 0,
@@ -23,9 +23,15 @@ class App extends Component {
     };
   }
 
+  timeOut(num){
+    this.shakeScreen();
+    window.setTimeout(() => {
+      this.startGame();
+    }, num);
+  }
+
   startGame(){
     if ( this.state.gameHasStarted){ return; }
-    this.shakeScreen();
 
     this.setState({
       gameHasStarted: true,
@@ -44,7 +50,7 @@ class App extends Component {
   }
 
   clearMoles(){
-    for(var value in this.state){
+    for(let value in this.state){
       if (!isNaN(value)){
         this.setState({
           [value]: 'translate(0, 110%)'
@@ -54,14 +60,14 @@ class App extends Component {
   }
 
   displayMoles(){
-    var activeMole = Math.ceil(Math.random() * 9);
+    let activeMole = Math.ceil(Math.random() * 9);
     if (this.state.lastMole[0] === activeMole){
       this.displayMoles();
       return;
     }
     this.clearMoles()
     this.setState({
-      [activeMole]: 'translate(0, 10%)',
+      [activeMole]: 'translate(0, 15%)',
       lastMole: [activeMole]
     });
   }
@@ -82,54 +88,89 @@ class App extends Component {
   }
 
   shakeScreen(){
-    console.log('shake');
+    let posOrNeg = '+';
+    let i = 0;
+    let shake = () => {
+      if (i === 15){
+        this.setState({ shake: 'translate(0, 0)' });
+        return;
+      }
+      window.setTimeout(() => {
+        posOrNeg = posOrNeg === '-' ? '+' : '-';
+        this.setState({ shake: `translate(${posOrNeg}${i}px, 0)` });
+        shake();
+      }, 80);
+      i++
+    };
+    shake();
   }
 
   render() {
     return (
       <div className="main-container">
-        <div className="game">
+        <div className="game" style={{transform: this.state['shake']}}>
           <h1 className="game__title">WHACK-A-MOLE</h1>
-          <button className="game__start-button" type="button" onClick={ this.startGame.bind(this) }>
-            Start Game!</button>
-          <div className="game__score">
-            <h1>Score: {this.state.score}</h1>
+          <div className="game__button-container">
+            <button className="game__start-button" type="button" onClick={ this.timeOut.bind(this, 800) }>
+              Start Game!
+            </button>
           </div>
           <div className="game__hole">
             <div className={"game__mole mole-1"} onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['1']}}></div>
+              style={{transform: this.state['1']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-2" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['2']}}></div>
+              style={{transform: this.state['2']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-3" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['3']}}></div>
+              style={{transform: this.state['3']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-4" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['4']}}></div>
+              style={{transform: this.state['4']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-5" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['5']}}></div>
+              style={{transform: this.state['5']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-6" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['6']}}></div>
+              style={{transform: this.state['6']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-7" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['7']}}></div>
+              style={{transform: this.state['7']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-8" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['8']}}></div>
+              style={{transform: this.state['8']}}>
+            </div>
+            <div className="game__mound"></div>
           </div>
           <div className="game__hole">
             <div className="game__mole mole-9" onClick={ this.addToScore.bind(this) }
-              style={{transform: this.state['9']}}></div>
+              style={{transform: this.state['9']}}>
+            </div>
+            <div className="game__mound"></div>
+          </div>
+          <div className="game__score">
+            <h1>SCORE: {this.state.score}</h1>
           </div>
         </div>
       </div>
